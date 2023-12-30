cd35187f1d062992da669a1a3108eb6f8c00df10
Nelson-Musonda <2020061767@student.unza.zm>
2023-12-30 08:49:29 +0200

    Improved responsiveness and layout of pages

--- script.js
+++ script.js
@@ -1,57 +1,58 @@
 let counter = 0;
-
 function addFiles() {
-	var pdfI = document.getElementById("pdfI" + counter);
-	var metaI = document.getElementById("metaI" + counter);
+	let pdfI = document.getElementById("pdfI" + counter);
+	let metaI = document.getElementById("metaI" + counter);
 
 	if (pdfI.files.length > 0 && metaI.files.length > 0) {
 		const br = document.createElement("br");
 		const tableCont = document.getElementById("files-list");
 		if (counter == 0){
 			document.getElementById("listMsg").textContent = "Selected Files";
-			tableCont.appendChild(listMsg);
 			tableCont.appendChild(br);
 		} 
-		const pdfIn = pdfI.files;
-		var p = pdfIn[0];
-		const metaIn = metaI.files;
-		var m = metaIn[0];
+		const pdfFile = pdfI.files[0];
+		const metaFile = metaI.files[0];
 
-		var table = document.createElement("table");
-		var newRow = table.insertRow(-1);
-		var cell1 = newRow.insertCell(0);
-		var cell2 = newRow.insertCell(1);
+		const table = document.createElement("table");
+		const newRow = table.insertRow(-1);
+		const cell1 = newRow.insertCell(0);
+		const cell2 = newRow.insertCell(1);
 		cell1.className = "file-cell";
 		cell2.className = "file-cell";
 		cell1.style.width = "50%";
 		cell2.style.width = "60%";
+		
+		cell1.style.fontSize = "130%";
+		cell2.style.fontSize = "130%";
+		
 		cell2.style.paddingLeft = "10%";
-		cell1.innerHTML = p.name;
-		cell2.innerHTML = m.name;
+		cell1.innerHTML = pdfFile.name;
+		cell2.innerHTML = metaFile.name;
 		table.style.width = "92%";
 		table.style.tableLayout = "fixed";
 		tableCont.appendChild(table);
 		tableCont.appendChild(document.createElement("hr"));
 		
+		const uploadForm = document.getElementById("fileInputForm");
 
-		const parent = document.getElementById("inputForm");
+		let nextCounter = counter + 1;
+		const pdfL = document.getElementById("pdfL");
+		pdfL.setAttribute("for", "pdfI" + nextCounter);
+		const oldPdfI = document.getElementById("pdfI" + counter);
+		const newPdfI = oldPdfI.cloneNode(true);
+		newPdfI.id = "pdfI" + nextCounter;
+		newPdfI.value = "";
 
-		var pdfOld = document.getElementById("pdfL");
-		pdfOld.setAttribute("for", "pdfI" + (counter + 1));
-		pdfOld = document.getElementById("pdfI" + counter);
-		var pdfNew = pdfOld.cloneNode(true);
-		pdfNew.id = "pdfI" + (counter + 1);
-		pdfNew.value = "";
-		parent.insertBefore(pdfNew, pdfOld.nextSibling);
+		uploadForm.appendChild(newPdfI);
 
-		var metaOld = document.getElementById("metaL");
-		metaOld.setAttribute("for", "metaI" + (counter + 1));
-		metaOld = document.getElementById("metaI" + counter);
-		var metaNew = metaOld.cloneNode(true);
-		metaNew.id = "metaI" + (counter + 1);
-		metaNew.value = "";
+		const metaL = document.getElementById("metaL");
+		metaL.setAttribute("for", "metaI" + nextCounter);
+		const oldMetaI = document.getElementById("metaI" + counter);
+		const newMetaI = oldMetaI.cloneNode(true);
+		newMetaI.id = "metaI" + nextCounter;
+		newMetaI.value = "";
+		uploadForm.appendChild(newMetaI);
 
-		parent.insertBefore(metaNew, metaOld.nextSibling);
 		counter++;
 	} else {
 		alert("please select files for both inputs to continue");
@@ -60,58 +61,91 @@ function addFiles() {
 
 
 function show_pane(){
-	document.getElementById("left_pane").style.left = "0px";
-		document.getElementById("show_p").style.display = "none";
-		document.getElementById("hide_p").style.display = "block";
-	document.getElementById("main").style.paddingLeft = '265px';
+	document.getElementById("left_pane").style.left = "0vw";
+	document.getElementById("show_p").style.display = "none";
+	document.getElementById("hide_p").style.display = "block";
+	document.getElementById("main").style.paddingLeft = '30vw';
 }
 
 function hide_pane(){
-	document.getElementById("left_pane").style.left = "-250px";
+	document.getElementById("left_pane").style.left = "-23vw";
+	const width = getBtnWidth("hide_p");	
 	document.getElementById("show_p").style.display = "block";
+	document.getElementById("show_p").style.height = width + "vw";
 	document.getElementById("hide_p").style.display = "none";
-	document.getElementById("main").style.paddingLeft = '105px';
+	document.getElementById("main").style.paddingLeft = '20vw';
 }
 
 
 function use_api(){
 	document.getElementById("default").style.display = "none";
 	document.getElementById("file_sect").style.display = "none";
-	document.getElementById("left_pane").style.left = "-250px";
+	document.getElementById("left_pane").style.left = "-23vw";
 	document.getElementById("show_p").style.display = "block";
+	const width = getBtnWidth("show_p");
+	document.getElementById("show_p").style.height = width + "vw";
 	document.getElementById("hide_p").style.display = "none";
-	document.getElementById("api_sect").style.display = 'block';
-	document.getElementById("main").style.paddingLeft = '105px';
+	document.getElementById("api_sect").style.display = "block";
+	document.getElementById("main").style.paddingLeft = "20vw";
 }
 
 function f_upload(){
 	document.getElementById("default").style.display = "none";
 	document.getElementById("api_sect").style.display = "none";
-	document.getElementById("left_pane").style.left = "-250px";
+	document.getElementById("left_pane").style.left = "-23vw";
 	document.getElementById("show_p").style.display = "block";
+	const width = getBtnWidth("hide_p");
+	document.getElementById("show_p").style.height = width + "vw";
 	document.getElementById("hide_p").style.display = "none";
 	document.getElementById("file_sect").style.display = 'block';
-	document.getElementById("main").style.paddingLeft = '105px';
+	document.getElementById("main").style.paddingLeft = '20vw';
 }
 
 function usage(){
 	document.getElementById("default").style.display = "block";
 	document.getElementById("api_sect").style.display = "none";
-	document.getElementById("left_pane").style.left = "-250px";
+	document.getElementById("left_pane").style.left = "-23.5%";
 	document.getElementById("show_p").style.display = "block";
+	const width = getBtnWidth("hide_p");
+	document.getElementById("show_p").style.height = width + "vw";
 	document.getElementById("hide_p").style.display = "none";
 	document.getElementById("file_sect").style.display = 'none';
-	document.getElementById("main").style.paddingLeft = '105px';
+	document.getElementById("main").style.paddingLeft = '20vw';
 }
 
 function dwnld(){
 	document.getElementById("default").style.display = "none";
 	document.getElementById("dwnld_sect").style.display = "block";
 	document.getElementById("api_sect").style.display = "none";
-	document.getElementById("left_pane").style.left = "-250px";
+	document.getElementById("left_pane").style.left = "-23vw";
 	document.getElementById("show_p").style.display = "block";
+	const width = getBtnWidth("hide_p");
+	document.getElementById("show_p").style.height = width + "vw";
 	document.getElementById("hide_p").style.display = "none";
 	document.getElementById("file_sect").style.display = 'none';
-	document.getElementById("main").style.paddingLeft = '105px';
+	document.getElementById("main").style.paddingLeft = '20vw';
 }
 
+function squareIt(arg){
+	const btns = document.querySelectorAll(arg);
+
+	// Loop through each button and set its height
+	btns.forEach(function(btn) {
+	// Get the button's width
+		let btnWidth = window.getComputedStyle(btn).width;
+		btnWidth = parseFloat(btnWidth);
+		btnWidth = (btnWidth / window.innerWidth) *100;
+
+		// Set the button's height to match its width
+		btn.style.height = btnWidth + "vw";
+	});
+}
+
+function getBtnWidth(arg){
+	const btn  = document.getElementById(arg);
+	let btnWidth = window.getComputedStyle(btn).width;
+	btnWidth = btnWidth / window.innerWidth;
+	btnWidth = btnWidth * 100;
+	
+	return btnWidth;
+}
